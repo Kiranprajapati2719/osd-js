a95a78450e49503e837bff175559aecc309ab72a
thapasalav <salav.rarasoft@gmail.com>
2019-07-22 09:59:16 +0545

    redesign bookview

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Bookview from './bookview/Bookview';
+// import Listview from './bookview/Listview';
 
 function App() {
   return (
     <div className="App">
       <Bookview />
     </div>
+    // <div className="App">
+    //   <Listview />
+    // </div>
+
   );
 }
 
