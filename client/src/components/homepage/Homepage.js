1830d419b43b183423db8ce0edb8ed26fb844fd7
ShyrushShrestha <shyrush.sresta1@gmail.com>
2019-08-24 14:23:42 +0545

    homepage design initiated

--- client/src/components/homepage/Homepage.js
+++ client/src/components/homepage/Homepage.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import './Homepage.css';
+import img1 from '../image/girlBook.svg';
+import img2 from '../image/logo.jpg';
+export default () =>
+    (
+        <>
+            <div class="headingPage">
+                <div className="bannerText">
+                    <h1>Find your book & lose yourself</h1>
 
-export default () => (
-<h1>
-    Homepage
-</h1>
-);
+                </div>
+
+                <div className="bannerImage">
+                    <img src={img1} className="banner" alt='book' />
+
+                </div>
+            </div>
+
+        </>
+    );
